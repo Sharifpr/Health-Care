8472280a8a38d68dd088596294e401ab982d1d96
Sharifpr <sharifpr532@gmail.com>
2021-10-19 23:15:39 +0600

    added

--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -7,7 +7,7 @@ import './Contact.css'
 const Contact = () => {
     return (
         <div className="row contact-container container mx-auto">
-            <div className="col-lg-5">
+            <div className="col-lg-5 my-5">
                 <h2>Contact Us</h2>
                 <p>Time and health are two precious assets that we don't recognize and appreciate until they have been depleted.</p>
                 <Link className="button-style" to="/Contact">Contact</Link>
